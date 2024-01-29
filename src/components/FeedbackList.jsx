5fb491e1bd6708a58be0848ac7693d21023448f7
Bashar Alwarad <beelwarad52@gmail.com>
2024-01-29 13:01:04 +0100

    frammer-motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,12 +1,22 @@
 import FeedbackItem from './FeedbackItem';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const FeedbackList = ({ feedbackData, handelDelete }) => {
   return (
-    <>
-      {feedbackData?.map((e) => (
-        <FeedbackItem key={e.id} item={e} handelDelete={handelDelete} />
-      ))}
-    </>
+    <div className='feedback-list'>
+      <AnimatePresence>
+        {feedbackData?.map((e) => (
+          <motion.div
+            key={e.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem item={e} handelDelete={handelDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
   );
 };
 
